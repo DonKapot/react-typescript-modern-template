31f7887a7effed34b8122ea98d60b3afdb2428f8
DonKapot <alexdanin@mail.ru>
2021-08-08 00:08:00 +0300

    Add typed eventHandlers and hook example

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -5,23 +5,56 @@ interface Children {
   head: ReactNode;
   body: ReactNode;
 }
-
 interface Props {
   message?: string;
   children?: Children;
+}
+interface MessageState {
+  hovered: string;
+  innerMessage: string;
 }
 
 const Message: React.FC<Props> = ({
   message = 'Default message',
   children = {} as Children,
-}): React.ReactElement => {
+}): JSX.Element => {
+  const initialMessageState: MessageState = {
+    hovered: '',
+    innerMessage: message,
+  };
+
+  const [messageState, setMessgeState] =
+    React.useState<MessageState>(initialMessageState);
+
+  const { hovered, innerMessage } = messageState;
+
   const { head, body } = children;
 
+  // typing on RIGHT hand side of =
+  const onHover = (e: React.MouseEvent<HTMLElement>): void => {
+    setMessgeState({ ...messageState, hovered: ' hovered' });
+  };
+
+  // typing on LEFT hand side of =
+  const onHoverOut: React.MouseEventHandler<HTMLElement> = () => {
+    setMessgeState({ ...messageState, hovered: '' });
+  };
+
+  const onChangeMessage: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setMessgeState({ ...messageState, innerMessage: e.currentTarget.value });
+  };
+
   return (
-    <section className="Message">
+    <section
+      className={`Message${hovered}`}
+      onMouseEnter={onHover}
+      onMouseLeave={onHoverOut}
+    >
       <div>{head}</div>
       <div>{body}</div>
-      <div>{message}</div>
+      <div>Prop Msg: {message}</div>
+      <div>State Msg: {innerMessage}</div>
+      <input value={innerMessage || ''} onChange={onChangeMessage} />
     </section>
   );
 };
